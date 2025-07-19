c21800364e7883ba648f096e86b5aca472f2e5c6
David Lachut <dslachut@gmail.com>
2025-07-19 18:00:35 -0500

    now chats with LLM

--- hawat/dispatcher.py
+++ hawat/dispatcher.py
@@ -1,7 +1,9 @@
+import hawat.language as language
 import hawat.memory as memory
 
 
 def process_message(message):
     # Call the new memory function to record the message
     memory.record_user_message(message)
-    return message
+    response = language.get_chat_completion(message)
+    return response
