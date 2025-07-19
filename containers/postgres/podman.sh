ccf7a3935fe90c3016f66947e5398a6bb57f6974
David Lachut <dslachut@gmail.com>
2025-07-18 21:29:42 -0500

    correct podman script for local postgres container

--- containers/postgres/podman.sh
+++ containers/postgres/podman.sh
@@ -4,6 +4,9 @@ podman build -t hawat-pg . ;
 
 podman run \
     --name hawat-db \
-    -e POSTGRES_PASSWORD_FILE=/etc/secrets \
-    -v /custom/mount:/var/lib/postgresql/data
+    -v ~/.pgdata/hawat:/var/lib/postgresql/data \
+    -v ~/.secrets:/etc/secrets \
+    -e POSTGRES_PASSWORD_FILE=/etc/secrets/pgpwf \
+    -e POSTGRES_USER=hawat \
+    -p 5431:5432 \
     -d hawat-pg
