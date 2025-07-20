df8f90441232a2b7805a85e1a7caadaa8d8192e9
David Lachut <dslachut@gmail.com>
2025-07-19 22:12:39 -0500

    Context sending corrected

--- hawat/dispatcher.py
+++ hawat/dispatcher.py
@@ -8,8 +8,8 @@ def process_message(message):
     conversational_context = memory.get_immediate_conversational_context()
     relevant_messages = memory.get_relevant_messages_by_vector_similarity(message)
 
-    formatted_conversational_context = [f"{{m[0]}} ({{m[2]}} minutes ago): {{m[1]}}" for m in conversational_context]
-    formatted_relevant_messages = [f"{{m[0]}} ({{m[2]}} minutes ago): {{m[1]}}" for m in relevant_messages]
+    formatted_conversational_context = [f"{m[0]} ({m[2]} minutes ago): {m[1]}" for m in conversational_context]
+    formatted_relevant_messages = [f"{m[0]} ({m[2]} minutes ago): {m[1]}" for m in relevant_messages]
 
     context = "\n".join(formatted_conversational_context + formatted_relevant_messages)
     response = language.get_chat_completion(message, context)
