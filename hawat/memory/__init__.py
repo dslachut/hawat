6e27cb329b1c8a3dfd2ecaf13ce5f6b12c4f1608
David Lachut <dslachut@gmail.com>
2025-08-01 14:00:29 -0500

    moved a function around

--- hawat/memory/__init__.py
+++ hawat/memory/__init__.py
@@ -1,8 +1,9 @@
 from hawat.memory.context import (
+    format_message_log,
     get_formatted_context,
     get_immediate_conversational_context,
     get_relevant_messages_by_vector_similarity,
 )
 from hawat.memory.conversations import get_current_conversation_id
-from hawat.memory.messages import record_message, format_message_log
+from hawat.memory.messages import record_message
 from hawat.memory.schema import get_connection_pool
